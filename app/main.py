b72873eefeef34cdbe7d9c97fdda01254b4f3fc1
kajianaLeVrai999 <hei.kajiana@gmail.com>
2025-09-03 12:16:32 +0300

    b et c

--- app/main.py
+++ app/main.py
@@ -1,7 +1,26 @@
 from fastapi import FastAPI
+from typing import List
+from pydantic import BaseModel
 
 app = FastAPI()
 
 @app.get("/ping")
 def ping():
     return {"message": "pong"}
+
+class CarModel (BaseModel):
+    identifier : str
+    brand : str
+    modele : str
+    characteristics : object
+
+cars_list : List[CarModel] = []
+
+@app.post("/cars")
+def new_cars(car_playload: List[CarModel]):
+    cars_list.extend(car_playload)
+    return {"cars": new_cars}
+
+@app.get("/cars")
+def list_cars():
+    return {"cars" : cars_list}
